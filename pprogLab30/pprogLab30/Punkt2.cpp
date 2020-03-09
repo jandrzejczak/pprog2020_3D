f2f49cc4feed0a759556b639078b23e60a495247
Jordan Andrzejczak <jordi.andrzejczak@gmail.com>
2020-03-09 10:53:53 +0100

    Dodane operatory +, -, *

--- pprogLab30/pprogLab30/Punkt2.cpp
+++ pprogLab30/pprogLab30/Punkt2.cpp
@@ -20,9 +20,9 @@ void Punkt2::setX(double _x) { x = _x; }
 
 void Punkt2::setY(double _y) { y = _y; }
 
-double Punkt2::getX() { return x; }
+double Punkt2::getX() const { return x; }
 
-double Punkt2::getY() { return y; }
+double Punkt2::getY() const { return y; }
 
 double Punkt2::getRadius() {
     return sqrt(pow(x, 2) + pow(y, 2));
@@ -34,6 +34,16 @@ double Punkt2::getAngle() {
 
 double Punkt2::getDistance(Punkt2 _p) {
     return sqrt(pow(_p.x - x, 2) + pow(_p.y - y, 2));
+}
+
+Punkt2 Punkt2::operator*(const Punkt2 &a) const{
+    return Punkt2(x * a.getX(), y * a.getY());
+}
+Punkt2 Punkt2::operator+(const Punkt2 &a) const{
+    return Punkt2(x + a.getX(), y + a.getY());
+}
+Punkt2 Punkt2::operator-(const Punkt2 &a) const{
+    return Punkt2(x - a.getX(), y - a.getY());
 }
 
 int Punkt2::getCountClasses() {return countClasses;}
